7629f8302d60f0c2b3227835b464dfc6e683379d
wuyifan <yifanwu@pku.edu.cn>
2020-06-18 16:30:15 +0800

    add parameter for Parse.

--- src/main/java/templatemining/Parse.java
+++ src/main/java/templatemining/Parse.java
@@ -45,7 +45,7 @@ public class Parse extends KeyedProcessFunction<String, Tuple2<String, String>, 
         Node rootNode = parseTree.value() == null || Config.valueStates.get("templateMap") == 1? tcfgUtil.getParseTreeRegion() : parseTree.value();
         Config.valueStates.put("templateMap",0);
         String[] regex = parameterTool.get("regex").split("&");
-        int depth = 4;
+        int depth = parameterTool.getInt("depth");
         int maxChild = 100;
         double st = 0.5;
         LogParser parser = new LogParser(regex, parameterTool.get("logFormat"), depth, maxChild, st);
